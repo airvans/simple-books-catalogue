985a62df91c218c31254330d85ef93b5b6342d3a
airvans <evanarugu@gmail.com>
2026-08-23 20:22:49 +0200

    feat: add theme toggle and search functionality with event listeners

--- src/main.js
+++ src/main.js
@@ -1,7 +1,38 @@
 
 
-function getBooks(){
+const elements = {
+    searchInput: document.querySelector('#search-input'),
+    searchButton: document.querySelector('#search-button'),
+};
 
-    
-    
+function binders() {
+    elements.searchButton.addEventListener('click', getBooks);
 }
+
+
+function getBooks() {
+
+    let value = elements.searchInput.value.trim();
+
+    console.log(encodeURIComponent(value));
+
+    const url = `https://openlibrary.org/search.json?q=${encodeURIComponent(value)}&limit=10`;
+
+    const headers = new Headers({
+        "User-Agent": "simple-books-catalog/1.0 (evan.della24@gmail.com)"
+    });
+
+    const options = {
+        method: 'GET',
+        headers: headers
+    };
+
+
+
+}
+
+function init() {
+    binders();
+}
+
+init();
